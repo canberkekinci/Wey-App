de222f77aa2ca2507cff7faf7a1531baeabbbb14
canberkekinci <1canberkekinci@gmail.com>
2025-09-16 18:18:18 +0300

    Added Sign Up and Home views. Also added Navbar component.

---
+++ src/views/HomeView.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1 class="text-2xl">Hello!</h1>
+</template>
---
+++ src/views/SignUpView.vue
@@ -0,0 +1,66 @@
+<script setup></script>
+
+<template>
+  <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
+    <div class="main-left col-span-2">
+      <div class="p-12 bg-white border border-gray-200 rounded-lg">
+        <h1 class="mb-6 text-2xl">Sign Up</h1>
+
+        <p class="mb-6 text-gray-500">
+          Lorem ipsum dolor sit mate. Lorem ipsum dolor sit mate. Lorem ipsum dolor sit mate. Lorem
+          ipsum dolor sit mate. Lorem ipsum dolor sit mate. Lorem ipsum dolor sit mate.
+        </p>
+
+        <p class="font-bold">
+          Already have an account? <a href="#" class="underline">Click here</a> to log in!
+        </p>
+      </div>
+    </div>
+
+    <div class="main-center col-span-2 space-y-4">
+      <div class="p-12 bg-white border border-gray-200 rounded-lg">
+        <form class="space-y-6">
+          <div>
+            <label>Name</label><br />
+            <input
+              type="name"
+              placeholder="Your full name"
+              class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"
+            />
+          </div>
+
+          <div>
+            <label>E-mail</label><br />
+            <input
+              type="email"
+              placeholder="Your e-mail address"
+              class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"
+            />
+          </div>
+
+          <div>
+            <label>Password</label><br />
+            <input
+              type="password"
+              placeholder="Your password"
+              class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"
+            />
+          </div>
+
+          <div>
+            <label>Repeat Password</label><br />
+            <input
+              type="password"
+              placeholder="Repeat your password"
+              class="w-full mt-2 py-4 px-6 border border-gray-200 rounded-lg"
+            />
+          </div>
+
+          <div>
+            <button class="py-4 px-6 bg-purple-600 text-white rounded-lg">Sign up</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
